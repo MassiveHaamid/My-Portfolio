1d5ad2fd12502620f405459bb44a964d8f27ca7c
hamiz <reshma9298h@gmail.com>
2025-02-05 23:13:24 +0530

    @property css update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,27 +11,27 @@ const Home = () => {
         <div className="col-md-6 home-details">
           {/* Add the image inside a circle */}
           <div className="profile-image-container">
-            <img src="/Pro Mode Active.png" alt="Haamid" className="profile-image" />
+          <img src="/Pro Mode Active.png" alt="Haamid" className="profile-image" />
           </div>
-          <h1 className="twinkling-gradient">
+          <h1>
             <span>Hello, I AM</span>
             <br />
             <div className="title-my-name">Haamid</div>
           </h1>
           <h2>Frontend and Backend Developer</h2>
           <hr />
-          <p className="medium twinkling-gradient">
+          </div>
+          <p className="medium">
             <i>
-              Looking for a skilled MERN Stack Developer to bring your web ideas to life? I'm Hamid, 
-              with a B.sc, in Computer Science and over 3 years of experience. I specialize in MongoDB, 
-              Express, React, and Nodejs delivering full-stack solutions with intuitive designs 
-              and secure systems. I completed a full-stack course through GUVI at the IITM Research Park. 
-              Let's collaborate to create high-quality, responsive web applications that turn your vision 
-              into reality!
+            Looking for a skilled MERN Stack Developer to bring your web ideas to life? I'm Hamid, 
+            with a B.sc, in Computer Science and over 3 years of experience. I specialize in MongoDB, 
+            Express, React, and Nodejs delivering full-stack solutions with intuitive designs 
+            and secure systems. I completed a full-stack course through GUVI at the IITM Research Park. 
+            Let's collaborate to create high-quality, responsive web applications that turn your vision 
+            into reality!
             </i>
           </p>
         </div>
-      </div>
     </article>
   );
 };
