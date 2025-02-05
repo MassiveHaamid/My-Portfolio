aaeefa86762b991f591b5502818b9e015809279d
hamiz <reshma9298h@gmail.com>
2025-02-06 00:23:37 +0530

    @property updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,32 +8,38 @@ const Home = () => {
       style={{ display: "block", paddingBottom: "40px" }}
     >
       <div className="home-page">
-      <div className="profile-top">
-        {/* <div className="col-md-6 home-details"> */}
+        <div className="profile-top">
           {/* Add the image inside a circle */}
           <div className="profile-image-container">
-          <img src="/Pro Mode Active.png" alt="Haamid" className="profile-image" />
-          </div><br /><br />
+            <img
+              src="/Pro Mode Active.png"
+              alt="Haamid"
+              className="profile-image"
+            />
+          </div>
+          <br />
+          <br />
           <h1>
             <span>Hello, I'M</span>
             <br />
-            <div className="title-my-name">Haamid</div><br />
+            <div className="title-my-name">Haamid</div>
+            <br />
           </h1>
           <h2>Frontend and Backend Developer</h2>
           <hr />
-          </div>
-          {/* </div> */}
-          <p className="medium">
-            <i>
-            Looking for a skilled MERN Stack Developer to bring your web ideas to life? I'm Hamid, 
-            with a B.sc, in Computer Science and over 3 years of experience. I specialize in MongoDB, 
-            Express, React, and Nodejs delivering full-stack solutions with intuitive designs 
-            and secure systems. I completed a full-stack course through GUVI at the IITM Research Park. 
-            Let's collaborate to create high-quality, responsive web applications that turn your vision 
-            into reality!
-            </i>
-          </p>
         </div>
+        <p className="medium">
+          <i>
+            Looking for a skilled MERN Stack Developer to bring your web ideas
+            to life? I'm Hamid, with a B.sc, in Computer Science and over 3
+            years of experience. I specialize in MongoDB, Express, React, and
+            Nodejs delivering full-stack solutions with intuitive designs and
+            secure systems. I completed a full-stack course through GUVI at the
+            IITM Research Park. Let's collaborate to create high-quality,
+            responsive web applications that turn your vision into reality!
+          </i>
+        </p>
+      </div>
     </article>
   );
 };
